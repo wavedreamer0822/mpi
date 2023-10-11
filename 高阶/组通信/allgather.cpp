0af4cf9682b4f5c9ed5d4ed9d779a7f7a2a26dfa
lipc <lipc19@outlook.com>
2023-10-11 09:32:19 +0800

    pty

--- 高阶/组通信/allgather.cpp
+++
@@ -1,9 +0,0 @@
-int MPI_Allgather(
-    void * sendbuf,         // 发送缓冲区的起始地址
-    int  sendcount,         // 向每个进程发送的数据个数
-    MPI_Datatype sendtype,  // 发送数据类型
-    void * recvbuf,         // 接收缓冲区的起始地址
-    int recvcount,          // 接收数据的个数
-    MPI_Datatype recvtype,  // 接收数据的类型
-    MPI_Comm comm           // 通信域
-)  //相当于每个进程执行了一次MPI_Gather()
